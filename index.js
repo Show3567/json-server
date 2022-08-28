e2ac0db0fc7e1292e2398ed7c85fdf1ea40eee32
e.unurbayar83@gmail.com <e.unurbayar83@gmail.com>
2022-08-28 01:54:30 -0500

    fixed

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ const View = (() => {
     course: "#availbeleCourse",
     button: "#sbtn",
     course2: "#selectedCourse",
-    // totalcredit: "#total"
   };
 
   const render = (ele, tmp) => {
     ele.innerHTML = tmp;
-    console.log(tmp);
   };
 
   const createTmp = (arr) => {
@@ -22,8 +20,8 @@ const View = (() => {
             <li class="courseItem" id="${course.courseId}">
               <span class="name">${course.courseName}</span>
               <span>Course type: ${
-                    course.required ? "Cumpolsery" : "Elective"
-                    }</span>
+                course.required ? "Cumpolsery" : "Elective"
+              }</span>
               <span>Course credit: ${course.credit}</span>
             </li>
             
@@ -54,17 +52,16 @@ const Model = ((api, view) => {
 
   class State {
     #courseList = [];
-    
+
     get courseList() {
       return this.#courseList;
     }
     set courseList(newlist) {
       this.#courseList = [...newlist];
-     
+
       const ulcontainer = document.querySelector(view.domstr.course);
       const tmp = view.createTmp(this.#courseList);
       view.render(ulcontainer, tmp);
-    
     }
   }
 
@@ -81,26 +78,21 @@ const Controller = ((model, view) => {
   let selectedCourses = [];
   let totalcredit = 0;
 
-
   const selectBtn = () => {
     const button = document.querySelector(view.domstr.button);
     button.addEventListener("click", (event) => {
-      console.log("clicked", totalcredit);
-      if(totalcredit<18){
-      let lis = document.querySelectorAll("#availbeleCourse li");
-      for (let i = 0;i< lis.length; i++) { 
-        let name = lis[i].getElementsByClassName('name')[0].innerHTML;
-        let t = lis[i].innerHtml;
-        console.log("name",name);
-        selectedCourses.forEach((selCourse)=>{
-            console.log("selCourse",selCourse.courseName)
-             if(selCourse.courseName===name)
-             lis[i].parentNode.removeChild(lis[i])
-         });
+      if (totalcredit < 18) {
+        let lis = document.querySelectorAll("#availbeleCourse li");
+        for (let i = 0; i < lis.length; i++) {
+          let name = lis[i].getElementsByClassName("name")[0].innerHTML;
+          let t = lis[i].innerHtml;
+          selectedCourses.forEach((selCourse) => {
+            if (selCourse.courseName === name)
+              lis[i].parentNode.removeChild(lis[i]);
+          });
         }
-    }
-    
-    
+      }
+
       const ulcontainer = document.querySelector(view.domstr.course2);
       const tmp = view.createTmp(selectedCourses);
       view.render(ulcontainer, tmp);
@@ -112,36 +104,32 @@ const Controller = ((model, view) => {
     ulcontainer.addEventListener("click", (event) => {
       if (event.target.classList.contains("courseItem")) {
         let classList = event.target.classList;
-        console.log(classList)
         let course = state.courseList.find(
           (elem) => elem.courseId == event.target.id
         );
 
         if (classList.contains("selected")) {
           selectedCourses = selectedCourses.filter((elem) => {
-            // console.log(elem.courseId, course.courseId);
             return elem.courseId != course.courseId;
           });
-          // console.log(selectedCourses);
           totalcredit -= course.credit;
           event.target.classList.remove("selected");
         } else {
-        //   console.log(course);
+          //   console.log(course);
           // if(totalcredit + course.credit === 12){
           //     alert(`
           //     You have chosen ${totalcredit} credits for semester.
           //     You cannot change once you submit.
           //     Do you want to confirm?`);
           // }
-          // else 
-          if(totalcredit + course.credit >18) {
+          // else
+          if (totalcredit + course.credit > 18) {
             alert("You cannot choose more than 18 credits in one semester!");
           } else {
             selectedCourses.push(course);
             totalcredit += course.credit;
             event.target.classList.add("selected");
             document.getElementById("total").innerHTML = totalcredit;
-            
           }
         }
         const ulcontainer = document.querySelector(view.domstr.course2);
@@ -162,7 +150,6 @@ const Controller = ((model, view) => {
     init();
     selectCourse();
     selectBtn();
-   
   };
   return {
     bootstrap,
