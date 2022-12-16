b8a141cdd2e1708a452f3e9a5908e59e7c1b051a
brianshkim <brianshkim25@gmail.com>
2022-12-16 15:02:23 -0800

    more changes

--- script.js
+++ script.js
@@ -7,35 +7,21 @@ const Api = (() => {
     const baseUrl = 'http://localhost:4232';
     const todoPath = 'courseList';
   
-    const getTodos = () =>
+    const getCourses = () =>
       fetch([baseUrl, todoPath].join('/')).then((response) => response.json());
   
-    const deleteTodo = (id) =>
-      fetch([baseUrl, todoPath, id].join('/'), {
-        method: 'DELETE',
-      }); // still need to maintain data deletion in the frontend
-  
-    const addTodo = (todo) =>
-      fetch([baseUrl, todoPath].join('/'), {
-        method: 'POST',
-        body: JSON.stringify(todo),
-        headers: {
-          'Content-type': 'application/json',
-          charset: 'UTF-8',
-        },
-      }).then((response) => response.json());
+
   
     return {
-      getTodos,
-      deleteTodo,
-      addTodo,
+      getCourses,
+  
     };
   })();
   
   const View = (() => {
     const domstr = {
-      todocontainer: '#todolist_container',
-      inputbox: '.todolist__input',
+      todocontainer: '#courses_container',
+      
     };
     // innerHTML
     
@@ -43,9 +29,10 @@ const Api = (() => {
       let tmp = '';
       arr.forEach((course) => {
         tmp += `
-        <li id=${course.courseId} } class="class-select-box">
+        <li id=${course.courseId}  class="class-select-box">
             <span>${course.courseName}</span>
             ${course.required?"<div>Compulsory</div>":"<div>Elective</div>"}
+            <div id=credits-${course.credit}>Credits: ${course.credit}</div>
             
         </li>
         `;
@@ -65,38 +52,82 @@ const Api = (() => {
 
       const myList =  document.querySelectorAll(".class-select-box")
       const counternumber = document.getElementById("counter-number")
+      const selected = document.getElementById("selected-courses-container")
+      const clickbutton = document.getElementById("confirmselect")
+      
       counternumber.innerHTML = counter
     
    
       myList.forEach(el=>{
           el.addEventListener('click', function handleClick(event) {
-              console.log(el.style.backgroundColor === "#ADD8E6")
+              
+              let getcredits = Number(el.childNodes[5].id.split("-")[1])
+              
               if(el.style.backgroundColor === "rgb(173, 216, 230)" && el.id % 2 ===0){
                 el.style.backgroundColor = "rgb(221, 239, 221)"
-                if(counter != 0) counter--
+                if(counter != 0) counter-=getcredits
                 counternumber.innerHTML = counter
                 
               }
               else if(el.style.backgroundColor === "rgb(173, 216, 230)" && el.id % 2 ===1){
                 el.style.backgroundColor = "rgb(255, 255, 255)"
-                if(counter != 0) counter--
+                if(counter != 0) counter-=getcredits
                 counternumber.innerHTML = counter
                 
               }
               else{
                 
                 el.style.backgroundColor = "#ADD8E6"
-                counter++
+                counter+=getcredits
                 counternumber.innerHTML = counter
+                let newel = el
+                
                 
               }
             });
 
             
           
-
-          
         });
+
+        clickbutton.addEventListener('click', function buttonClick(event){
+
+          if(counter > 0 && counter <= 18){
+          alert("You have chosen " + counter + " credits for this semester. You cannot change once you submit. Do you want to confirm?")
+          myList.forEach(el=>{
+                
+                
+                if(el.style.backgroundColor === "rgb(173, 216, 230)" ){
+                  selected.append(el)
+                  
+                  if (el.id%2===0){
+                    el.style.backgroundColor="rgb(221, 239, 221)"
+                  }
+                  else if(el.id%2===1){
+                    el.style.backgroundColor="rgb(255,255,255)"
+                  }
+
+                  
+
+                  
+              };
+
+            });
+            clickbutton.disabled = true;
+  
+              
+            
+          }
+
+          else if(counter > 18){
+            alert("You can only choose up to 18 credits in one semester")
+          }
+          
+        
+        });        
+
+
+        
 
     };
   
@@ -108,7 +139,7 @@ const Api = (() => {
   })();
   
   const Model = ((api, view) => {
-    const { getTodos, deleteTodo, addTodo } = api;
+    const { getCourses} = api;
   
     class Todo {
       constructor(title) {
@@ -143,9 +174,8 @@ const Api = (() => {
     
   
     return {
-      getTodos,
-      deleteTodo,
-      addTodo,
+      getCourses,
+    
       State,
       Todo,
     };
@@ -163,16 +193,11 @@ const Api = (() => {
    
   
     const init = () => {
-      model.getTodos().then((todos) => {
+      model.getCourses().then((todos) => {
         state.todoList = todos;
       });
     };
 
-
-    
-
-
-  
    
   
     const bootstrap = () => {
